d1031fe17f98f14901d8eefb63e2184795d20ce7
Peter Smith <peter@cruelandunusual.be>
2024-09-07 15:33:50 +0100

    making sprites move with keyboard input

--- 04_CreatingGameworld_SpriteDrawing/SpriteDrawing.cs
+++ 04_CreatingGameworld_SpriteDrawing/SpriteDrawing.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
 using System;
 
 namespace SpriteDrawing
@@ -11,12 +13,20 @@ namespace SpriteDrawing
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D balloon;
+        Vector2 balloonPos;
+        Color background;
+        float spriteX;
+        float spriteY;
 
      
         public SpriteDrawing()
         {
             Content.RootDirectory = "Content";
             graphics = new GraphicsDeviceManager(this);
+            balloonPos = Vector2.Zero;
+            spriteX = 0f;
+            spriteY = 0f;
+    
         }
 
         protected override void LoadContent()
@@ -27,11 +37,53 @@ namespace SpriteDrawing
             //MediaPlayer.Play(Content.Load<Song>("snd_music"));
         }
 
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                spriteX -= 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                spriteX += 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                spriteY -= 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                spriteY += 2;
+            }
+
+            /*
+            //calculate background colour
+            int backgroundComponent_RED = gameTime.TotalGameTime.Minutes * 2000;
+            int backgroundComponent_GREEN = gameTime.TotalGameTime.Seconds * 300;
+            int backgroundComponent_BLUE = gameTime.TotalGameTime.Milliseconds;
+            background = new Color(
+                backgroundComponent_RED,
+                backgroundComponent_GREEN,
+                backgroundComponent_BLUE);
+            */
+            float xPosition = (graphics.PreferredBackBufferHeight / 2) + (gameTime.TotalGameTime.Milliseconds / 2) + spriteX;
+            //float yPosition = (graphics.PreferredBackBufferHeight / 2) - (gameTime.TotalGameTime.Milliseconds / 2) + spriteY;
+            float yPosition = (graphics.PreferredBackBufferHeight / 2) + spriteY;
+            balloonPos = new Vector2(xPosition, yPosition);
+        }
+
         protected override void Draw(GameTime gameTime)
         {
+            Console.WriteLine("test");
+            Debug.WriteLine("hello");
             GraphicsDevice.Clear(Color.White);
+
             spriteBatch.Begin();
-            spriteBatch.Draw(balloon, Vector2.Zero, Color.White);
+            spriteBatch.Draw(balloon, balloonPos, Color.White);
             spriteBatch.End();
         }
     }
