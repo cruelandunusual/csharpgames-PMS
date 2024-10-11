9ec72ca9cd25e684564b621bb19cc3668222c835
Peter Smith <peter@cruelandunusual.be>
2024-10-11 17:32:52 +0100

    Added InputHelper and Gameworld classes, modified them with my own method for quitting the game

---
+++ 07_BasicGameObjects_Painter4/Painter.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Painter
+{
+    class Painter : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        InputHelper inputHelper;
+        static GameWorld gameWorld;
+
+        public Painter()
+        {
+            Content.RootDirectory = "Content";
+            graphics = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
+            inputHelper = new InputHelper();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            gameWorld = new GameWorld(Content);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            inputHelper.Update();
+            gameWorld.QuitIfEscape(inputHelper, this);
+            gameWorld.HandleInput(inputHelper);
+            gameWorld.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            gameWorld.Draw(gameTime, spriteBatch);
+        }
+
+        public static GameWorld GameWorld
+        {
+            get { return gameWorld; }
+        }
+
+    }
+}
