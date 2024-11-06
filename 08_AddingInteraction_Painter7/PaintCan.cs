f7e8b0fe4f16834a9e91ada0f1e69477c291f645
Peter Smith <peter@cruelandunusual.be>
2024-11-06 10:25:02 +0000

    nothing major

--- 08_AddingInteraction_Painter7/PaintCan.cs
+++ 08_AddingInteraction_Painter7/PaintCan.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
@@ -12,11 +13,12 @@ namespace Painter
         Color color, targetcolor;
         float minVelocity;
 
+
         public PaintCan(ContentManager Content, float positionOffset, Color targetcol)
         {
-            colorRed = Content.Load<Texture2D>("spr_can_red");
-            colorGreen = Content.Load<Texture2D>("spr_can_green");
-            colorBlue = Content.Load<Texture2D>("spr_can_blue");
+            this.colorRed = Content.Load<Texture2D>("spr_can_red");
+            this.colorGreen = Content.Load<Texture2D>("spr_can_green");
+            this.colorBlue = Content.Load<Texture2D>("spr_can_blue");
             targetcolor = targetcol;
             minVelocity = 30;
             color = Color.Blue;
@@ -24,6 +26,7 @@ namespace Painter
             velocity = Vector2.Zero;
         }
 
+        // TODO: calculate cannon ball intersecting with paint can
         public void Update(GameTime gameTime)
         {
             if (velocity.Y == 0.0f && Painter.Random.NextDouble() < 0.01)
@@ -33,6 +36,7 @@ namespace Painter
             }
             position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            //TODO: more investigations of how the position of the ball's centre is calculated
             Vector2 distanceVector = (Painter.GameWorld.Ball.Position + Painter.GameWorld.Ball.Center) - (position + Center);
             if (Math.Abs(distanceVector.X) < Center.X && Math.Abs(distanceVector.Y) < Center.Y)
             {
@@ -45,6 +49,7 @@ namespace Painter
                 Reset();
             }
             minVelocity += 0.001f;
+            Debug.WriteLine(minVelocity);
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -86,6 +91,10 @@ namespace Painter
                 return Color.Blue;
         }
 
+        // TODO: change the name of this property...
+        // thoroughly investigate what happens when
+        // the game uses the current texure colour 
+        // instead of the Color property
         public Color Color
         {
             get { return color; }
