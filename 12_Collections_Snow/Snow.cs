ae9b15968115e5cfadc5e6247ebc7b2c8611faff
Peter Smith <peter@cruelandunusual.be>
2025-01-19 17:27:31 +0000

    Snow game - collections and iterating through them

--- 12_Collections_Snow/Snow.cs
+++ 12_Collections_Snow/Snow.cs
@@ -14,19 +14,19 @@ namespace Snow
         protected GraphicsDeviceManager graphics;
         protected SpriteBatch spriteBatch;
 
-        protected static Random random;
-        protected static Point screen;
+        protected static Random random; // static Random variable accessible by other classes in namespace
+        protected static Point screen; // static Point variable accessible by other classes in namespace
 
         private InputHelper inputHelper;
 
         Texture2D background, snowflake;
-        List<Snowflake> snowflakes;
+        IList<Snowflake> snowflakes; // declare instance variable of List of type Snowflake
 
         public Snow()
         {
             Content.RootDirectory = "Content";
             graphics = new GraphicsDeviceManager(this);
-            snowflakes = new List<Snowflake>();
+            snowflakes = new List<Snowflake>(); // initialise the list object
             random = new Random();
             inputHelper = new InputHelper();
             IsMouseVisible = true;
@@ -37,11 +37,13 @@ namespace Snow
             spriteBatch = new SpriteBatch(GraphicsDevice);
             screen = new Point(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
             background = Content.Load<Texture2D>("spr_background");
-            snowflake = Content.Load<Texture2D>("spr_snowflake");
+            snowflake = Content.Load<Texture2D>("spr_snowflake"); // initialise the Texture2D snowflake variable
+            Debug.WriteLine("snowflakes.Count = " + snowflakes.Count);
             while (snowflakes.Count < 500)
             {
-                snowflakes.Add(new Snowflake(snowflake));
+                snowflakes.Add(new Snowflake(snowflake)); // populate the list, passing the Texture2D to the Snowflake constructor
             }
+            Debug.WriteLine("snowflakes.Count = " + snowflakes.Count);
         }
 
         protected override void Update(GameTime gameTime)
@@ -59,10 +61,20 @@ namespace Snow
             GraphicsDevice.Clear(Color.White);
             spriteBatch.Begin();
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
-            foreach (Snowflake obj in snowflakes)
+            //foreach (Snowflake obj in snowflakes)
+            //{
+            //    obj.Draw(gameTime, spriteBatch);
+            //}
+            int loopCount = 0;
+            for (int i = snowflakes.Count-1; i >= 0; i--)
             {
-                obj.Draw(gameTime, spriteBatch);
+                if (i % 2 == 0)
+                {
+                    snowflakes[i].Draw(gameTime, spriteBatch);
+                    loopCount++;
+                }
             }
+            Debug.WriteLine("loopCount = " + loopCount);
             spriteBatch.End();
         }
 
